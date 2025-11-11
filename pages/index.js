6119cfd3b968b77ad3091c1b16a65492443d7258
Monteiro <correiomon1eiro@gmail.com>
2025-11-11 00:19:34 +0000

    adiciona página inicial altera frase da página inicial

---
+++ pages/index.js
@@ -0,0 +1,7 @@
+// Home começa com H maiusculo convenção react
+// peça para IA explicar o meu código
+function Home() {
+    return <h1>Tainá, eu amo você. Se você me ama, dá uma risadinha</h1>
+} 
+//é assim que o next chama os componentes react
+export default Home;
